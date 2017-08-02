71e7a6c4b3e5fd344c0dfed793c06bedae40995c
bbbht <plateau.loess@gmail.com>
2017-08-02 10:34:43 +0800

    使用.env文件管理变量，添加php7.1版本

--- mysql/run/init.sh
+++ mysql/run/init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if [ -f "$WORK_PATH/$INIT_SQL" ]; then
+if [ -f "$WORK_PATH/$MYSQL_INIT_SQL" ]; then
 mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOF
 use $MYSQL_DATABASE;
-source $WORK_PATH/$INIT_SQL;
+source $WORK_PATH/$MYSQL_INIT_SQL;
 EOF
+# 只执行一次
+mv $WORK_PATH/$MYSQL_INIT_SQL $WORK_PATH/$MYSQL_INIT_SQL.bak
 else
-    echo 'nothing exist' /tmp/init.sh.log
+    echo 'no sql file exist' > /dev/null
 fi
